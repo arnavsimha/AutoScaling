b2ca08bba024a5ce75f39ea91af22e0672063a81
arnavsimha <arnavsimha@users.noreply.github.com>
2018-06-22 11:52:48 -0500

    Update and rename YamlReader.py to AutoScaler.py

---
+++ AutoScaler.py
@@ -0,0 +1,31 @@
+import yaml
+
+def get_account_info():
+
+    api_url = '{0}account'.format(api_url_base)
+
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        return json.loads(response.content.decode('utf-8'))
+    else:
+        return None
+
+#print yaml file
+with open("example.yaml", 'r') as stream:
+        try:
+            YML = yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+#iterate through Yaml file
+from ruamel.yaml import YAML
+
+yaml = YAML()
+input_file = 'dev-creds-newcert.yml'
+
+for key, value in YML.items():
+    print(str(key))
+
+
+
+#search 
--- YamlReader.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-#print yaml file
-with open("example.yaml", 'r') as stream:
-    try:
-        print(yaml.load(stream))
-    except yaml.YAMLError as exc:
-        print(exc)
