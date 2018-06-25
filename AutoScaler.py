ebcee00b7b5fede47a5bae3de3b5854e367df7f8
arnavsimha <arnavsimha@users.noreply.github.com>
2018-06-25 15:00:51 -0500

    Update AutoScaler.py

--- AutoScaler.py
+++ AutoScaler.py
@@ -11,3 +11,17 @@ for elem in data:
          elem['externalWorkerInstances'] = instances_desired
          break  # no need to iterate further
 yaml.dump(data, sys.stdout)
+
+
+#alternative implementation
+/*
+with open("my_file.yaml") as f:
+     list_doc = yaml.load(f)
+
+for sense in list_doc:
+    if sense["name"] == "sense2":
+         sense["value"] = 1234
+
+with open("my_file.yaml", "w") as f:
+    yaml.dump(list_doc, f)
+*/
