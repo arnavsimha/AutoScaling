54609deb0577afa6c161a8b11298585a867d5ffe
arnavsimha <arnavsimha@users.noreply.github.com>
2018-06-25 15:02:34 -0500

    Update AutoScaler.py

--- AutoScaler.py
+++ AutoScaler.py
@@ -4,7 +4,7 @@ import ruamel.yaml
 instances_desired = 0 #from scaleUp/scaleDown function
 yaml = ruamel.yaml.YAML()
 # yaml.preserve_quotes = True
-with open('dev-creds-newcert.yaml') as fp:
+with open('dev-creds-newcert.yml') as fp:
     data = yaml.load(fp)
 for elem in data:
     if elem['name'] == 'info':
@@ -14,14 +14,14 @@ yaml.dump(data, sys.stdout)
 
 
 #alternative implementation
-/*
-with open("my_file.yaml") as f:
+"""
+with open("dev-creds-newcert.yml") as f:
      list_doc = yaml.load(f)
 
 for sense in list_doc:
-    if sense["name"] == "sense2":
-         sense["value"] = 1234
+    if sense["name"] == "info":
+         sense["externalWorkerInstances"] = instances_desired
 
-with open("my_file.yaml", "w") as f:
+with open("dev-creds-newcert.yml", "w") as f:
     yaml.dump(list_doc, f)
-*/
+"""
