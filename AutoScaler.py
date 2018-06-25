79f1f7eba7cce64313e5744b02a17a5c584198fd
arnavsimha <arnavsimha@users.noreply.github.com>
2018-06-25 14:57:32 -0500

    Update AutoScaler.py

--- AutoScaler.py
+++ AutoScaler.py
@@ -1,6 +1,7 @@
 import sys
 import ruamel.yaml
 
+instances_desired = 0 #from scaleUp/scaleDown function
 yaml = ruamel.yaml.YAML()
 # yaml.preserve_quotes = True
 with open('dev-creds-newcert.yaml') as fp:
